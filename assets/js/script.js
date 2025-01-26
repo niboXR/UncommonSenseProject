02cb09168499f98b469979b68c7281b2c563e7d9
niboXR <mytrashgeemail@gmail.com>
2025-01-26 11:25:24 -0500

    Auto-update

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = resizeFooter;
-
-function resizeFooter() {
-    var footer = document.getElementById('sticky-footer');
-    footer.style.height = 'calc(100vh - ' + (window.innerHeight - footer.offsetTop) + 'px)';
-}
